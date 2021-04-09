17a9144f85d9cd534471bcf3032b46e419724bd6
shina2012 <shina88hn@gmail.com>
2021-04-09 22:09:45 +0700

    new update

---
+++ Session7/index.js
@@ -0,0 +1,13 @@
+// overflow: 
+// cursor: là thay đổi hình dạng con trỏ 
+// .content :hover {
+       // thay đổi con trỏ khi di chuyển vào vị trí content
+// }
+
+// z-index: làm các block nổi và chìm
+// transfrom :translateX(-50%) căn thẻ X Y Z , size thẻ - scale
+// transition: thời gian để hoàn thành hành động hover
+//transition-timing-funtion: ease-in , ease-out , liear
+// box-shadow : ccsmatic.com tìm hiểu
+
+//

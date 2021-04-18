0c9fd16e05060a7516cb38755fe9b1d8f63c4255
shina2012 <shina88hn@gmail.com>
2021-04-18 19:49:34 +0700

    homework1

---
+++ Homework1/index.js
@@ -0,0 +1,39 @@
+// Bài 1;
+// những trường hợp sẽ báo syntaxerror là khi
+// Các biến bắt đầu bằng số_trùng keyword của js_chứa các ký tự đặc biệt'
+// ví dụ "let Char" "console.log('200k'),console.log('@1000')'
+// dùng toàn tử typeof để kiểm tra kiểu dữ liệu của biến bất kỳ trong js'
+// Bài 2;
+// đáp án 1-b 2-a 3-d 4-c'
+// Bài 3;
+let message = 'Coding is great';
+console.log(message);
+let studentCount = 0
+console.log(studentCount);
+// Bài 4;
+message = 'Coding might not be easy, but still great';
+console.log(message);
+// Bài 5;
+studentCount = 16
+console.log(studentCount);
+let doi;
+doi = message.toLowerCase();
+console.log(doi);
+// Bài 6;
+++studentCount;
+console.log(studentCount);
+// Bài 7;
+let a = prompt("Hi there,Your name please?");
+alert('Chào ' + a);
+let firstName = prompt("first name please?");
+let lastName = prompt("last name please?");
+alert('Chào '+ firstName  + lastName );
+// Bài 8;
+let cạnh = prompt("Nhập vào 1 cạnh hình vuông");
+alert(`Diện tích hình vuông là : ${cạnh * cạnh}`);
+// Bài 9;
+let circle = prompt("Nhập vào bán kính hình tròn");
+alert(`Diện tích hình tròn là : ${circle * circle * 3.14} `);
+// Bài 10;
+let độC = prompt("Nhập vào số độ C cần đổi");
+alert(`Độ F là : ${độF = (độC * 1.8) +32}`);
